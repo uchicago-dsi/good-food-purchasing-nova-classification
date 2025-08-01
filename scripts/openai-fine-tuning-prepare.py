57c6526ba5ba137028350cf05538b6ce4d7ad33c
Jim Pivarski <jpivarski@gmail.com>
2025-08-01 17:49:27 -0500

    try2: equal-sized datasets in all 4 categories

--- scripts/openai-fine-tuning-prepare.py
+++ scripts/openai-fine-tuning-prepare.py
@@ -3,6 +3,8 @@ import random
 
 import pandas as pd
 
+SIZE = 430
+
 dataset = pd.read_csv(
     "~/Box/dsi-core/11th-hour/good-food-purchasing/Filtered_OFF_with_sentences.csv",
     sep="\t",
@@ -10,7 +12,20 @@ dataset = pd.read_csv(
 )
 
 random.seed(12345)
-dataset = list(dataset.itertuples())
+
+dataset1 = list(dataset[dataset["nova_group"] == 1].itertuples())
+random.shuffle(dataset1)
+
+dataset2 = list(dataset[dataset["nova_group"] == 2].itertuples())
+random.shuffle(dataset2)
+
+dataset3 = list(dataset[dataset["nova_group"] == 3].itertuples())
+random.shuffle(dataset3)
+
+dataset4 = list(dataset[dataset["nova_group"] == 4].itertuples())
+random.shuffle(dataset4)
+
+dataset = list(dataset1[0 : 3*SIZE] + dataset2[0 : 3*SIZE] + dataset3[0 : 3*SIZE] + dataset4[0 : 3*SIZE])
 random.shuffle(dataset)
 
 # Encodings of desired outputs differ in only one token, the group integer.
@@ -24,7 +39,7 @@ random.shuffle(dataset)
 message_format = '{{"messages":[{{"role":"user","content":{ingredients}}},{{"role":"assistant","content":"{{\\\"nova_group\\\":{nova_group}}}"}}]}}\n'
 
 with open("training.jsonl", "w") as file:
-    for row in dataset[0:1000]:
+    for row in dataset[: 4*SIZE]:
         file.write(
             message_format.format(
                 ingredients=json.dumps(row.ingredients_text),
@@ -33,7 +48,7 @@ with open("training.jsonl", "w") as file:
         )
 
 with open("validation.jsonl", "w") as file:
-    for row in dataset[1000:2000]:
+    for row in dataset[4*SIZE : 8*SIZE]:
         file.write(
             message_format.format(
                 ingredients=json.dumps(row.ingredients_text),
@@ -42,7 +57,7 @@ with open("validation.jsonl", "w") as file:
         )
 
 with open("test.jsonl", "w") as file:
-    for row in dataset[2000:3000]:
+    for row in dataset[8*SIZE :]:
         file.write(
             message_format.format(
                 ingredients=json.dumps(row.ingredients_text),
